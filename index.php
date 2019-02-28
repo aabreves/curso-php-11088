009452ef01e1b6b5acbf4a259fc95ebf675bf528
aabreves <aa.breves@outlook.com>
2019-02-28 19:58:37 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CURSO PHP - 11088</title>
+	</head>
+
+	<body>
+		<?php
+			echo "<h4>CURSO PHP - 11088</h4>";
+			echo "<hr />";
+		?>
+		<ul>
+			<li>
+				<a href="classes/aula-2702.php" target="_self" >Aula 27/fev</a>
+			</li>
+		</ul>
+	</body>
+</html>
