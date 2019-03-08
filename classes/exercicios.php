ba54027ac014ebd8b536f5b6a77c619c8fcf97c1
aabreves <aa.breves@outlook.com>
2019-03-07 21:54:50 -0300

    Aula 07/mar

--- classes/exercicios.php
+++ classes/exercicios.php
@@ -1,35 +1,75 @@
-<?php
-?>
+<?php ?>
 <!DOCTYPE html>
 <html lang="pt-br">
-   <head>
-		<meta charset="utf-8" />
-  	<meta name="viewport"
-			content="width=device-width, initial-scale=1" />
-		<title>Aula 28/Fev</title>
-	</head>
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport"
+              content="width=device-width, initial-scale=1" />
+        <title>Aula 28/Fev</title>
 
-	<body>
-      <ul>
-      <?php
-         $sHref = "exercices/ex4-pag23-form.php";
-         $sTarget = "_blank";
-         $sCaption = "Exercício 4 da pag 23";
-         $sLink = '<a href="'.$sHref.'" target="'.$sTarget.'" >'.$sCaption.'</a>';
-         echo "<li>$sLink</li>";
+        <meta charset="utf-8" />
+        <meta name="viewport"
+              content="width=device-width, initial-scale=1" />
+        <link rel="stylesheet"
+              href="../node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        <script src="../node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="../node_modules/popper.js/dist/umd/popper.min.js"></script>
+        <script src="../node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    </head>
 
-         $sHref = "exercices/ex4-pag23-form-v2.php";
-         $sTarget = "_blank";
-         $sCaption = "Exercício 4 da pag 23 (versão 2)";
-         $sLink = '<a href="'.$sHref.'" target="'.$sTarget.'" >'.$sCaption.'</a>';
-         echo "<li>$sLink</li>";
+    <body style="width:100%;height:100%">
+        <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
+            <a class="navbar-brand" 
+               href="#">
+                <img src="../media/images/bird.jpg" 
+                     alt="Logo" 
+                     style="width:40px;">
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" 
+                       href="#">Link 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" 
+                       href="#">Link 2</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle"
+                       href="#" 
+                       id="navbardrop" 
+                       data-toggle="dropdown">
+                        Exercícios pag23
+                    </a>
+                    <ul class="dropdown-menu">
+                        <?php
+                        $sHref = "exercices/ex4-pag23-form.php";
+                        $sTarget = "ifrNav";
+                        $sCaption = "Exercício 4";
+                        $sLink = '<a class=\"nav-link\" href="'.$sHref.'" target="'.$sTarget.'" >'.$sCaption.'</a>';
+                        echo "<li class=\"dropdown-item\">$sLink</li>";
 
-         $sHref = "exercices/ex5-pag23-form.php";
-         $sTarget = "_blank";
-         $sCaption = "Exercício 5 da pag 23";
-         $sLink = '<a href="'.$sHref.'" target="'.$sTarget.'" >'.$sCaption.'</a>';
-         echo "<li>$sLink</li>";
-      ?>
-      </ul>
-	</body>
+                        $sHref = "exercices/ex4-pag23-form-v2.php";
+                        $sTarget = "ifrNav";
+                        $sCaption = "Exercício 4 (2)";
+                        $sLink = '<a class=\"nav-link\" href="'.$sHref.'" target="'.$sTarget.'" >'.$sCaption.'</a>';
+                        echo "<li class=\"dropdown-item\">$sLink</li>";
+
+                        $sHref = "exercices/ex5-pag23-form.php";
+                        $sTarget = "ifrNav";
+                        $sCaption = "Exercício 5";
+                        $sLink = '<a class=\"nav-link\" href="'.$sHref.'" target="'.$sTarget.'" >'.$sCaption.'</a>';
+                        echo
+                        "<li class=\"dropdown-item\">$sLink</li>";
+                        ?>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        <div style="position:absolute;top:60px;width:100%;height:600px;background-color:#000;padding:10px;">
+            <iframe id="ifrNav" 
+                    name="ifrNav"
+                    style="width:100%;height:100%;background-color:#aaa"></iframe>
+        </div>
+    </body>
 </html>
--- index.php
+++ index.php
@@ -1,27 +1,39 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>CURSO PHP - 11088</title>
-	</head>
+    <head>
+        <title>CURSO PHP - 11088</title>
 
-	<body>
-		<?php
-			echo "<h4>CURSO PHP - 11088</h4>";
-			echo "<hr />";
-		?>
-		<ul>
-			<li>
-				<a href="classes/aula-2702.php" target="_self" >Aula 27/fev</a>
-			</li>
-			<li>
-				<a href="classes/aula-2802.php" target="_self" >Aula 28/fev</a>
-			</li>
-			<li>
-				<a href="classes/aula-0103.php" target="_self" >Aula 01/mar</a>
-			</li>
-			<li>
-				<a href="classes/exercicios.php" target="_self" >Exercícios</a>
-			</li>
-		</ul>
-	</body>
+        <meta charset="utf-8" />
+        <meta name="viewport"
+              content="width=device-width, initial-scale=1" />
+<!--        <link rel="stylesheet"
+              href="node_modules/bootstrap/dist/css/bootstrap.min.css" />
+        <script src="node_modules/jquery/dist/jquery.min.js"></script>
+        <script src="node_modules/popper.js/dist/umd/popper.min.js"></script>
+        <script src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>-->
+    </head>
+
+    <body>
+        <?php
+        echo "<h4>CURSO PHP - 11088</h4>";
+        echo "<hr />";
+        ?>
+        <ul>
+            <li>
+                <a href="classes/aula-2702.php" target="_self" >Aula 27/fev</a>
+            </li>
+            <li>
+                <a href="classes/aula-2802.php" target="_self" >Aula 28/fev</a>
+            </li>
+            <li>
+                <a href="classes/aula-0103.php" target="_self" >Aula 01/mar</a>
+            </li>
+            <li>
+                <a href="classes/exercicios_old.php" target="_self" >Exercícios</a>
+            </li>
+            <li>
+                <a href="classes/exercicios.php" target="_self" >Exercícios</a>
+            </li>
+        </ul>
+    </body>
 </html>
