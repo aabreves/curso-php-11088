77c1a4c4e8fa6b97d6c9c5fbcb6764e562d7ddc3
aabreves <aa.breves@outlook.com>
2019-03-18 20:29:36 -0300

    Aula 18Mar - northwind_data.sql

--- classes/php-samples/session-3.php
+++ classes/php-samples/session-3.php
@@ -4,68 +4,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-class Menu{
-    private $id      = "";
-    private $caption = "";
-    private $itens   = [];
-    
-    public function __construct( $id, $caption ){
-        $this->id      = $id;
-        $this->caption = $caption;
-    }
-    
-    public function __destruct(){
-    }
+global $sSessionName;
 
-    public function addItem( $oItemMenu ){
-        $this->itens[] = $oItemMenu;
-    }
-    
-    public function count(){
-        return count( $this->itens );
-    }
-    
-    public function item( $iIndex ){
-        return $this->itens[$iIndex];
-    }
+session_start();
+
+if ( isset( $_SESSION["session-name"] ) ){
+    $sSessionName = $_SESSION["session-name"];
 }
-
-class ItemMenu{
-    private $href;
-    private $target;
-    private $caption;
-    
-    public function __construct( $href, $target, $caption ){
-        $this->href    = $href;
-        $this->target  = $target;
-        $this->caption = $caption;
-    }
-    
-    public function __destruct(){
-    }
+else{
+    $sSessionName = "undefined";
 }
-
-$oMenu = new Menu( "menu_2", "Menu 2" );
-//$oMenu->id      = "menu_2";
-//$oMenu->caption = "Menu 2";
-//$oMenu->itens   = [];
-
-$oItemMenu = new ItemMenu( "#", "_self", "Item 1" );
-//$oItemMenu->href    = "#";
-//$oItemMenu->target  = "_self";
-//$oItemMenu->caption = "Item 1";
-
-//$oMenu->itens[] = $oItemMenu;
-$oMenu->addItem( $oItemMenu );
-$oMenu->addItem( new ItemMenu( "#", "_self", "Item 2" ) );
-$oMenu->addItem( new ItemMenu( "#", "_self", "Item 3" ) );
-
 
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
     <head>
-        <title>Classes & Objetos</title>
+        <title>Session 2</title>
 
         <meta charset="utf-8" />
         
@@ -93,11 +47,10 @@ $oMenu->addItem( new ItemMenu( "#", "_self", "Item 3" ) );
     <body style="width:100%;height:100%">
         <div id="divContent">
             <?php
-                // TODO:                
-                echo "<h5>Objeto Menu - class Menu</h5>";
-                echo "<pre>";
-                print_r( $oMenu );
-                echo "</pre>";
+                // TODO:
+                session_destroy();
+                echo "<h5>PHP Session: $sSessionName</h5>";
+                echo "<h5>Session detroyed</h5>";
             ?>
         </div>
     </body>
